db1078280864e65707387f9129631d4f4ee2a225
Leo Leppänen <leo.leppanen@helsinki.fi>
2021-03-22 14:27:44 +0200

    Simplify TMDocsetComparison resource

--- explainer/resources/topic_model_document_set_comparison_resource.py
+++ explainer/resources/topic_model_document_set_comparison_resource.py
@@ -2,7 +2,7 @@ import logging
 from typing import List, Type
 
 from explainer.core.models import Fact, Message
-from explainer.core.realize_slots import RegexRealizer, SlotRealizerComponent
+from explainer.core.realize_slots import SlotRealizerComponent
 from explainer.explainer_message_generator import Event
 from explainer.resources.processor_resource import TaskResource
 
@@ -10,7 +10,7 @@ log = logging.getLogger("root")
 
 
 TEMPLATE = """
-en: A topic model {parameters} was used to compare corpora.
+en: A topic model was used to compare corpora.
 | name = TopicModelDocsetComparison
 """
 
@@ -24,40 +24,7 @@ class TopicModelDocumentSetComparisonResource(TaskResource):
         if not task or task.name != "TopicModelDocsetComparison":
             return []
 
-        return [
-            Message(
-                Fact(
-                    "task",
-                    "TopicModelDocsetComparison",
-                    "[TopicModelDocsetComparison:MODEL:NAME:{}] [TopicModelDocsetComparison:MODEL:TYPE:{}]".format(
-                        task.parameters.get("model_name"), task.parameters.get("model_type")
-                    ),
-                    event.id,
-                )
-            )
-        ]
+        return [Message(Fact("task", "TopicModelDocsetComparison", None, event.id,))]
 
     def slot_realizer_components(self) -> List[Type[SlotRealizerComponent]]:
-        return [EnglishTopicModelTypeRealizer, EnglishTopicModelNameRealizer]
-
-
-class EnglishTopicModelTypeRealizer(RegexRealizer):
-    def __init__(self, registry):
-        super().__init__(
-            registry,
-            "en",
-            r"(.*)\s?\[TopicModelDocsetComparison:MODEL:TYPE:([^\]]*)\]\s?(.*)",
-            (1, 2, 3),
-            "{} of the {} variety {}",
-        )
-
-
-class EnglishTopicModelNameRealizer(RegexRealizer):
-    def __init__(self, registry):
-        super().__init__(
-            registry,
-            "en",
-            r"(.*)\s?\[TopicModelDocsetComparison:MODEL:NAME:([^\]]*)\]\s?(.*)",
-            (1, 2, 3),
-            "{} called {} {}",
-        )
+        return []
